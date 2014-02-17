c51afb47c59f9b9b7d001f391f455b0e2ac481c6
James McKay <dcseee-github@yahoo.com.au>
2014-02-17 23:33:42 +1100

    Use the interface instead of the implementation for RF charging.
    Should fix zerotheliger/PowerConverters#21
    Tested all Redstone Arsenal tools, as well as items from
    * IC2, Gregtech, & Gravisuite
    * Applied energistics
    * Thermal expansion
    * Tinkers construct (with flux addition)
    * Modular powersuits
    All change when a vanilla chest is placed next to a universal charger with the items in it.

--- src/main/java/powercrystals/powerconverters/power/thermalexpansion/ChargeHandlerRF.java
+++ src/main/java/powercrystals/powerconverters/power/thermalexpansion/ChargeHandlerRF.java
@@ -4,7 +4,7 @@ import net.minecraft.item.ItemStack;
 import powercrystals.powerconverters.common.IChargeHandler;
 import powercrystals.powerconverters.mods.ThermalExpansion;
 import powercrystals.powerconverters.power.PowerSystem;
-import cofh.api.energy.ItemEnergyContainer;
+import cofh.api.energy.IEnergyContainerItem;
 
 /**
  * @author samrg472
@@ -21,21 +21,21 @@ public class ChargeHandlerRF implements IChargeHandler
     @Override
     public boolean canHandle(ItemStack stack)
     {
-	return stack.getItem() instanceof ItemEnergyContainer;
+	return stack.getItem() instanceof IEnergyContainerItem;
     }
 
     @Override
     public int charge(ItemStack stack, int energyInput)
     {
 	final int energyToUse = (int) (energyInput / getPowerSystem().getInternalEnergyPerOutput());
-	final int energyUsed = ((ItemEnergyContainer) stack.getItem()).receiveEnergy(stack, energyToUse, false);
+	final int energyUsed = ((IEnergyContainerItem) stack.getItem()).receiveEnergy(stack, energyToUse, false);
 	return (int) ((energyToUse - energyUsed) * getPowerSystem().getInternalEnergyPerOutput());
     }
 
     @Override
     public int discharge(ItemStack stack, int energyRequest)
     {
-	ItemEnergyContainer cell = (ItemEnergyContainer) stack.getItem();
+	IEnergyContainerItem cell = (IEnergyContainerItem) stack.getItem();
 	return (int) ((cell.extractEnergy(stack, (int) (energyRequest / getPowerSystem().getInternalEnergyPerOutput()), false)) * getPowerSystem().getInternalEnergyPerOutput());
     }
 }
