c6cb3d9b7eb3bf51ba7bdf8abf42fa6fd1f6d92f
James McKay <dcseee-github@yahoo.com.au>
2014-03-04 13:51:51 +1100

    Add message stating Rotarycraft support is disallowed

---
+++ src/main/java/powercrystals/powerconverters/mods/Rotarycraft.java
@@ -0,0 +1,4 @@
+/**
+ * Reika (the rotarycraft dev) has explicitly disallowed Rotarycraft support in Powerconverters.
+ * This is because of the damage that does to Rotarycraft's balance, design, realism, and interfacing.
+ */
