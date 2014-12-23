21cfcde5ad5d8d8a26e2cf860db448b8168ca988
Florian Wilkemeyer <fw@f-ws.de>
2014-12-23 16:49:49 +0100

    Changed EnderIO recipe to support the new Capacitor Bank - fixes #17

--- src/main/java/powercrystals/powerconverters/crafting/mods/RecipeEnderIO.java
+++ src/main/java/powercrystals/powerconverters/crafting/mods/RecipeEnderIO.java
@@ -2,6 +2,7 @@ package powercrystals.powerconverters.crafting.mods;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Configuration;
 import powercrystals.powerconverters.crafting.RecipeProvider;
@@ -18,13 +19,31 @@ public class RecipeEnderIO extends RecipeProvider{
         //To change body of implemented methods use File | Settings | File Templates.
         PowerSystem rf = PowerSystemManager.getInstance().getPowerSystemByName(PowerRedstoneFlux.id);
         if(rf != null) {
-            Block converterBlock = rf.block;
+            Block converterBlock = rf.block;            
+            
+            //
+            // Support both, new and old Capacitor bank
+            // (where the new is the most prefered one)
+            //
+            Item capBank = GameRegistry.findItem("EnderIO", "blockCapBank");
+            ItemStack stackCapacitorBank;
+            if(capBank != null) {
+	        	// New Style Capacitor Bank
+	            stackCapacitorBank = new ItemStack(capBank, 1, 2);	// Basic -> :1,  Normal -> :2, Vibrant -> :3
+
+    		} else {
+    			// Old Capacitor Bank
+    			stackCapacitorBank = GameRegistry.findItemStack("EnderIO", "blockCapacitorBank", 1);
+	    		
+    		}
+    		
+    		        
             GameRegistry.addRecipe(new ItemStack(converterBlock, 1, 0),
                     "G G",
                     " T ",
                     "G G",
                     'G', GameRegistry.findItem("minecraft", "gold_ingot"),
-                    'T', GameRegistry.findItemStack("EnderIO", "blockCapacitorBank", 1)
+                    'T', stackCapacitorBank
             );
         }
     }
